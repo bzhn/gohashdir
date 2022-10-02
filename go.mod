bf734380cd06192d53907ced46bd203ad9c1edb0
bzhn <bzhn@duck.com>
2022-10-02 23:02:56 +0300

    new functions, improved file path output
    I have to delete not used functions later

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/bzhn/gohashdir
+
+go 1.19
+
+require github.com/cespare/xxhash v1.1.0
--- main.go
+++ main.go
@@ -4,13 +4,62 @@ import (
 	"crypto/sha256"
 	"fmt"
 	"os"
-	"path"
+	"path/filepath"
 	"strconv"
+	"strings"
 	"sync"
 	"time"
 
 	"github.com/cespare/xxhash"
 )
+
+var (
+	reportOutputType = "list" // In the future it will be (or won't be) a flag item [list|json|table]
+)
+
+// ProcessedFile consists of all the data about
+// file that have been processed we need:
+// Full path to file
+// hash sums (sha256, xxhash64)
+// Size in bytes
+// Creation time
+// Change time
+type ProcessedFile struct {
+	Path        string
+	Sha256sum   string
+	Xxhash64sum string
+	Size        uint64
+	Created     time.Time
+	Changed     time.Time
+}
+
+func (pf ProcessedFile) String() string {
+	return fmt.Sprintf("Path:\t\t%s\nxxhash64:\t%s\nsha256:\t\t%s\nSize:\t\t%d\n\n", pf.Path, pf.Xxhash64sum, pf.Sha256sum, pf.Size)
+}
+
+type Report struct {
+	Files []ProcessedFile
+}
+
+// String returns string that contains full report
+// for the Report struct
+func (r Report) String() string {
+	var sRep string
+
+	switch strings.ToLower(reportOutputType) {
+	case "list":
+		for _, pf := range r.Files {
+			sRep += pf.String()
+		}
+	case "json":
+
+	case "table":
+	default:
+
+	}
+
+	return sRep
+}
 
 func main() {
 	var userPath string
@@ -20,6 +69,16 @@ func main() {
 	} else {
 		userPath = os.Args[1]
 	}
+
+	// Start test
+	f, d, err := dirContent(userPath)
+	checkErr(err)
+
+	rep := new(Report)
+	loopFiles(userPath, f, d, rep)
+	fmt.Println(*rep)
+	return
+	// End test
 
 	if de, err := os.Stat(userPath); !de.IsDir() {
 		checkErr(err)
@@ -61,12 +120,55 @@ func filesInDir(dn string) []os.DirEntry {
 	return fe
 }
 
+// dirContent returns the list of files in ([]os.DirEntry)
+// and a slice of strings that contains names of all directories
+// in the given path.
+// If the given path is a file, then error will be returned
+func dirContent(dn string) (files []os.DirEntry, directories []string, err error) {
+	fe, err := os.ReadDir(dn)
+	if err != nil {
+		return files, directories, err
+	}
+
+	for _, v := range fe {
+		if v.IsDir() {
+			directories = append(directories, v.Name())
+		} else {
+			files = append(files, v)
+		}
+	}
+
+	return
+}
+
+func loopFiles(dirPath string, files []os.DirEntry, folders []string, rep *Report) {
+
+	for _, de := range files {
+		fc, err := os.ReadFile(filepath.Join(dirPath, de.Name()))
+		checkErr(err)
+		pf := ProcessedFile{
+			Path:        filepath.Join(dirPath, de.Name()),
+			Sha256sum:   sha256Sum(fc),
+			Xxhash64sum: xxhash64Sum(fc),
+			Size:        uint64(len(fc)),
+		}
+		(*rep).Files = append((*rep).Files, pf)
+	}
+
+	for _, fn := range folders {
+		fullDirPath := filepath.Join(dirPath, fn)
+		fls, fldrs, err := dirContent(fullDirPath)
+		checkErr(err)
+		loopFiles(fullDirPath, fls, fldrs, rep)
+	}
+}
+
 func loopThroughFiles(dirPath string, des []os.DirEntry) {
 	var wg sync.WaitGroup
 	for _, de := range des {
 		wg.Add(1)
 		if de.IsDir() { // recursive call
-			dn := path.Join(dirPath, de.Name())
+			dn := filepath.Join(dirPath, de.Name())
 			loopThroughFiles(dn, filesInDir(dn))
 			wg.Done()
 			continue
@@ -74,7 +176,7 @@ func loopThroughFiles(dirPath string, des []os.DirEntry) {
 
 		go func(de os.DirEntry) { // base case, check file's hash
 			// Get file content
-			fc, err := os.ReadFile(path.Join(dirPath, de.Name()))
+			fc, err := os.ReadFile(filepath.Join(dirPath, de.Name()))
 			checkErr(err)
 			startAt := time.Now()
 
@@ -83,7 +185,7 @@ func loopThroughFiles(dirPath string, des []os.DirEntry) {
 			wg.Done()
 		}(de)
 		wg.Wait() // It probably removes the effect from concurrency
-		// I have to manage amount of bytes that are being reading in the current time and wait
+		// I have to manage amount of bytes that are being reading at the current time and wait
 		// if it exeeds certain value
 	}
 }
